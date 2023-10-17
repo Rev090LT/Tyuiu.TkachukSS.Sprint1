a88784607d2e88dc6e11d2d7168583238806f798
Rev090LT <mamontosaimon900@gmail.com>
2023-10-18 00:21:02 +0500

    Task5

---
+++ Tyuiu.TkachukSS.Sprint1.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TkachukSS.Sprint1.Task5.V0.Lib;
+
+namespace Tyuiu.TkachukSS.Sprint1.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService dataService = new DataService();
+            int k = 23, wait = 2, res = dataService.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TkachukSS.Sprint1.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TkachukSS.Sprint1.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TkachukSS.Sprint1.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9f4458b-301b-4d55-8c85-c0432559840e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
